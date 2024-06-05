2c770f3deae8cfaba829249aaf90678ff3c15288
Jorge Junior <jorge.junior.568@live.com>
2024-06-05 18:20:32 -0300

    fix: scalable no

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/logo.png" />
         <meta name="theme-color" content="#FFFFFF" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
+        />
       </Head>
       <body>
         <Main />
